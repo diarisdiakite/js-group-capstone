60d743d6e124ac56ddb310779dfee0e72cd57329
diarisdiakite <diarisdiakite@gmail.com>
2023-07-31 22:37:35 +0200

    Add project setup

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './assets/css/style.css';
+
+// const myFooter = document.getElementById('footer-section');
+// myFooter.innerHTML += footerText;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    fallback: {
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      util: require.resolve('util'),
+      zlib: require.resolve('browserify-zlib'),
+      stream: require.resolve('stream-browserify'),
+      url: require.resolve('url'),
+    },
+  },
+};
