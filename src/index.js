65d8122ae0535caccc12d07f21bbaac694cb419e
diarisdiakite <diarisdiakite@gmail.com>
2023-08-01 22:47:19 +0200

    Add items counter to the main page.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import './assets/css/style.css';
-import displayItemsMain from './modules/mainPage/displayItems.js';
+import displayItemsMain, { mainPageItemsCounter } from './modules/mainPage/displayItems.js';
 import footerText from './modules/globalElements/footer.js';
 
+const mainPageCounterDisplayHTML = document.getElementById('counter');
+const updateSelectedItemsCount = async () => {
+  const count = await mainPageItemsCounter();
+  mainPageCounterDisplayHTML.textContent = count;
+};
+
+window.addEventListener('load', updateSelectedItemsCount);
 displayItemsMain();
 
 const myFooter = document.getElementById('footer-section');
--- src/modules/mainPage/displayItems.js
+++ src/modules/mainPage/displayItems.js
@@ -2,13 +2,46 @@ import { returnItems } from '../globalElements/api.js';
 
 export const initializeItemsDivHTML = () => document.querySelector('.main-items');
 export const itemsDivHTML = initializeItemsDivHTML();
+const selectedItems = [];
 
+// Implementing the counter
+export const returnSelectedItems = async () => {
+  if (selectedItems.length === 0) {
+    try {
+      const items = await await returnItems();
+      // Adding the items dynamically
+      for (let i = 0; i < 24; i += 1) {
+        selectedItems.push(items[i]);
+      }
+      return selectedItems;
+    } catch (error) {
+      const errorMessage = `couldn't find the items. ${error}`;
+      return Promise.reject(errorMessage);
+    }
+  } else {
+    return selectedItems;
+  }
+};
+
+export const mainPageItemsCounter = async () => {
+  await returnSelectedItems();
+  return selectedItems.length;
+};
+
+(async () => {
+  const selectedItems = await returnSelectedItems();
+  return selectedItems.length;
+})();
+
+// Display Items
 const displayItemsMain = async () => {
   try {
     const items = await await returnItems();
     // Adding the items dynamically
     for (let i = 0; i < 24; i += 1) {
       const item = items[i];
+      selectedItems.push(items[i]);
+
       const itemCardHtml = document.createElement('div');
       itemCardHtml.classList.add('item');
       itemCardHtml.innerHTML = `
@@ -44,7 +77,7 @@ const displayItemsMain = async () => {
       itemsDivHTML.innerHtml += itemCardHtml;
       itemsDivHTML.appendChild(itemCardHtml);
     }
-    return items;
+    return selectedItems;
   } catch (error) {
     const errorMessage = `Couldn't display the items, ${error}`;
     return Promise.reject(errorMessage);
