c2ad0538867aae7ae55b4a0d68908db60d0f36da
diarisdiakite <diarisdiakite@gmail.com>
2023-08-01 04:42:05 +0200

    Add images. Rename buttons

--- src/modules/mainPage/displayItems.js
+++ src/modules/mainPage/displayItems.js
@@ -7,44 +7,41 @@ const displayItemsMain = async () => {
   try {
     const items = await await returnItems();
     // Adding the items dynamically
-    for (let i = 0; i < 10; i += 1) {
+    for (let i = 0; i < 24; i += 1) {
       const item = items[i];
       const itemCardHtml = document.createElement('div');
       itemCardHtml.classList.add('item');
       itemCardHtml.innerHTML = `
-                    
-        <div class="item-image">
-          <img class="img-placeholder" src=${item.image.meduim} alt="item-image" placeholder="image for item 1">
-          </img>
-        </div>
+
+        <img class="item-image" 
+          src=${item.image.medium} 
+          alt="item-image" 
+          placeholder="image for item 1">
+        </img>
         
-        <div class="item-wrapped-text">
-          <div class="sections-title">
-            <h3>
-              ${item.name}
-            </h3>
-            <p>
-              ${item.language}
-            </p>
-          </div>
-          <button class="items-buttons" data-item-index="${i}" id="itemPopup">See item</button>
+        
+        <div class="item-wrapped-elements">
+          <h3>
+            ${item.name}
+          </h3>
+          <p>
+            ${item.language}
+          </p>
         </div>
-        <div id="item-image" class="item-image"></div>
-      </div>`;
+          <button class="items-buttons" data-item-index="${i}" id="itemPopup">Comments</button>
+        </div>`;
 
-      /* const itemImage = document.createElement('div');
-        itemImage.setAttribute('blur', '100');
+      const itemImage = document.createElement('div');
+      itemImage.setAttribute('blur', '100');
 
-        if (window.innerWidth < 768) {
-          itemImage.src = `${item.mobileImageUrls[0]}`;
-          itemCardHtml.appendChild(itemImage);
-          // itemCardHtml.classList.add('js-mobile-itemCards');
-        } else {
-          itemImage.src = `${item.desktopImageUrls[0]}`;
-          itemCardHtml.appendChild(itemImage);
-          // itemCardHtml.classList.add('js-desktop-itemCards');
-        }
-        itemsDivHTML.innerHtml += itemCardHtml; */
+      if (window.innerWidth < 768) {
+        itemImage.src = `${item.image.medium}`;
+        itemCardHtml.appendChild(itemImage);
+      } else {
+        itemImage.src = `${item.image.original}`;
+        itemCardHtml.appendChild(itemImage);
+      }
+      itemsDivHTML.innerHtml += itemCardHtml;
       itemsDivHTML.appendChild(itemCardHtml);
     }
     return items;
